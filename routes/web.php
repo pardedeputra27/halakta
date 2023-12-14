82db974539f5ea2a4e4ba7d928f474fdaef81092
pardedeputra27 <pardedeputra27@gmail.com>
2023-12-14 20:58:54 +0700

    add : Butterfly Bootstrap Template

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return User::all();
-    // return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('/');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
